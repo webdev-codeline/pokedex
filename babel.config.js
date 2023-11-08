1319d5a399b1bed1a9eef4aa0238c35f78de5c34
webdev-codeline <rain116093@hotmail.com>
2023-11-08 07:44:48 -0600

    ✨ Feat: fixed path aliases

--- babel.config.js
+++ babel.config.js
@@ -10,8 +10,21 @@ module.exports = function (api) {
           root: ['.'],
           alias: {
             // This needs to be mirrored in tsconfig.json
+
             '@': './src',
+            '@assets': './src/assets',
             '@components': './src/components',
+            '@features': './src/features',
+            '@graphql': './src/graphql',
+            '@hooks': './src/hooks',
+            '@lib': './src/lib',
+            '@navigation': './src/navigation',
+            '@redux': './src/redux',
+            '@schemas': './src/schemas',
+            '@screens': './src/screens',
+            '@stories': './src/stories',
+            '@types': './src/types',
+            '@utils': './src/utils',
           },
         },
       ],
