41f3cba92173cc1de642140c49a1a11d441cff0e
webdev-codeline <rain116093@hotmail.com>
2023-11-08 07:44:47 -0600

    🔥 Fix: removed web packages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 // react
 import React from 'react';
-import { Text } from 'react-native';
 
 // libs
-import { ApolloProvider } from '@apollo/client';
 import Constants from 'expo-constants';
 
 // configs
-import client from './client';
+// import client from './client';
 
 // styles
-import { SafeView } from './App.styles';
-import { Demo } from './src/components/Demo';
-import { IconFiles, IconNames } from './src/utils/iconNames.helper';
 import { Details } from './src/screens/Details';
-import { SvgProps } from 'react-native-svg';
+import { IconFiles } from './src/utils/iconNames.helper';
 import { colors } from './src/utils/pokemonTypeColors.helper';
 
 function App() {
