0969e5ee9687e7274487a934fbfe5da983128570
webdev-codeline <rain116093@hotmail.com>
2023-11-08 07:44:47 -0600

    ✨ Feat: added sample api calls to demo component

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
 import { Text } from 'react-native';
-import { usePokemonSearchByNameQuery } from '../hooks';
-import { wrapIn } from '../utils/input.helper';
+import { usePokemonDecendentFindByIdQuery } from '../hooks';
 
 export const Demo: FC = () => {
-  const { data, loading } = usePokemonSearchByNameQuery({ variables: { name: wrapIn('%')('zard') } });
+  // const { data, loading } = usePokemonSearchByNameQuery({ variables: { name: wrapIn('%')('zard') } });
+  // const { data, loading } = usePokemonSearchByIdsQuery({ variables: { ids: [1, 2, 3] } });
+  // const { data, loading } = usePokemonFindByIdQuery({ variables: { id: 80 } });
+  const { data, loading } = usePokemonDecendentFindByIdQuery({ variables: { id: 85 } });
 
   if (loading) return <Text>Loading</Text>;
 
-  console.log(data?.pokemons);
+  // console.log(data?.pokemons);
+  console.log(data?.pokemon);
 
   return <Text>Fetched</Text>;
 };
